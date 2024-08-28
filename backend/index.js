9305b937187f442b9a8df6d986d5ff96e1ab9d05
Mohamed Guemri <mohamedamiineguemri@gmail.com>
2024-08-28 18:12:08 +0200

    build the project and start deployment

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
-import cors from "cors"
+import path from "path";
 
 import { connectDB } from "./DB/connectDB.js"
 
@@ -15,17 +15,21 @@ import { app, server } from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000
 
+const __dirname=path.resolve()
 app.use(express.json())
 app.use(cookieParser())
 dotenv.config()
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials : true
-// }))
+
 
 app.use("/api/auth",authRouter)
 app.use("/api/messages",messagesRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT, ()=>{
     connectDB()
