5458818137c8acaf0f69d72bda64a3bb34640580
Mohamed Guemri <mohamedamiineguemri@gmail.com>
2024-08-29 00:20:35 +0200

    style updates

--- frontend/src/componenets/messages/MessagesContainer.jsx
+++ frontend/src/componenets/messages/MessagesContainer.jsx
@@ -12,21 +12,28 @@ const MessagesContainer = () => {
   },[setSelectedConversation])
 
   return (
-    <div className="md:min-w-[450px] flex flex-col">
-    {
-      !selectedConversation? <NoChatSelected /> :
-      <>
-        {/* Header */}
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-          <span className="label-text text-gray-300">To: </span>
-          <span className="text-gray-300 font-bold">{selectedConversation.fullName}</span>
-        </div>
+    <div className="md:min-w-[450px] flex flex-col h-[100vh] max-h-[100vh]">
+  {
+    !selectedConversation ? <NoChatSelected /> :
+    <>
+      {/* Header */}
+      <div className="bg-slate-500 px-4 py-2">
+        <span className="label-text text-gray-300">To: </span>
+        <span className="text-gray-300 font-bold">{selectedConversation.fullName}</span>
+      </div>
 
+      {/* Messages */}
+      <div className="flex-grow overflow-y-auto">
         <Messages />
+      </div>
+
+      {/* Input */}
+      <div className="px-4 py-2 bg-slate-600">
         <MessageInput />
-      </>
-    }
-    </div>
+      </div>
+    </>
+  }
+</div>
   );
 };
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import MessagesContainer from "../../componenets/messages/MessagesContainer"
 
 const Home = () => {
   return (
-    <div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className='flex flex-col w-full max-h-[100vh] overflow-y-auto sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
     
 			<Sidebar />
 			<MessagesContainer />
