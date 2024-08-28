351f0cbf249f0ab357ce5eb8aa8d7b015641b94f
Mohamed Guemri <mohamedamiineguemri@gmail.com>
2024-08-28 18:36:29 +0200

    dev

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -4,8 +4,7 @@ import jwt from "jsonwebtoken"
     const token =  jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"})
     res.cookie("token",token,{
         httpOnly:true,
-        secure:process.env.NODE_ENV ==="production",
-        sameSite : "strict" ,
+        secure:process.env.NODE_ENV !== "development",
         maxAge : 7 * 24 * 60 * 60 * 1000
     })
 
